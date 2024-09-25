2817be0e6b8a8300ae37cbd49bfec133fdc9eb26
nzm-el <nizamel1402@gmail.com>
2024-09-25 08:01:25 +0700

    bissmillah ini yang kedua

--- PemilihanBilangan18.java
+++ PemilihanBilangan18.java
@@ -3,17 +3,13 @@ public class PemilihanBilangan18 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
     System.out.print("Masukan sebuah angka: ");
     int angka = sc.nextInt();
 
-    if (angka %2 == 0) 
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan genap");
-    }
-    else
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-    }
-    
+     String hasil = (angka %2 == 0 ) ? 
+     " bilangan genap" : " bilangan ganjil";
+    System.out.println("Angka " + angka + " adalah" + hasil);
+
     }
 }
